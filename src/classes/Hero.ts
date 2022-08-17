9c54aa90d5b21f31692f6c26c7c7f01f7733e34c
natogourmet <natogourmet@hotmail.com>
2022-08-17 14:27:57 -0500

    80-error-solving

--- src/classes/Hero.ts
+++ src/classes/Hero.ts
@@ -1,9 +1,16 @@
+import powers from '../data/powers';
+
 export class Hero {
   constructor(
     public name: string,
     public powerId: number,
     public alterEgo?: string
-  ) {
+  ) {}
 
+  get power(): string {
+    return (
+      powers.find((power) => power.id === this.powerId)?.description ||
+      'No power'
+    );
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 // import { Hero as Superhero } from "./classes/Hero";
 
-import * as HeroClasses from "./classes/Hero";
+import { Hero } from "./classes/Hero";
 import powers from './data/powers';
-
-const Hero = 'Hero';
 
 console.log('Hola Mundo!');
 
-const ironman = new HeroClasses.Hero('Ironman', 1, 'Tony Stark');
+const ironman = new Hero('Ironman', 1, 'Tony Stark');
 
 console.log(ironman);
-console.log(powers);
+console.log(ironman.power);
 
 
 
