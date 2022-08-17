5dc9bc00c1316a8e570990ec2d39a71be52d28d5
natogourmet <natogourmet@hotmail.com>
2022-08-17 14:45:15 -0500

    85-generics-example

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { genericFunction, printObject, genericArrowFunction } from './generics/generics';
+import { Hero } from './interfaces/Hero';
+import { Villain } from './interfaces/villain';
 
 // printObject( 123 );
 // printObject( new Date() );
@@ -7,3 +9,12 @@ import { genericFunction, printObject, genericArrowFunction } from './generics/g
 console.log( genericArrowFunction( 123 ).toFixed(2) );
 console.log( genericArrowFunction( new Date() ).getDate() );
 console.log( genericArrowFunction( 'Hello World' ).toUpperCase() );
+
+const deadpool = {
+  name: 'Deadpool',
+  realName: 'Wade Winston Wilson',
+  dangerLevel: 100,
+}
+
+console.log( genericArrowFunction<Hero>( deadpool ).realName );
+console.log( genericArrowFunction<Villain>( deadpool ).dangerLevel );
